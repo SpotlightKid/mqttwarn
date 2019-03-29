09ded02595d0b9ded26526a7bcf0f12bc5adece4
Christopher Arndt <chris@chrisarndt.de>
2019-03-29 20:31:11 +0100

    Fix Python 3 incompatibilities, outdated idioms, coding-style issues in 'sqlite' plugin; also make sqlite3 access more robust
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- mqttwarn/services/sqlite.py
+++ mqttwarn/services/sqlite.py
@@ -1,47 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
-__copyright__ = 'Copyright 2014 Jan-Piet Mens'
-__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+__author__ = "Jan-Piet Mens <jpmens()gmail.com>"
+__copyright__ = "Copyright 2014 Jan-Piet Mens"
+__license__ = "Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"
 
-HAVE_SQLITE=True
 try:
     import sqlite3
-except:
-    HAVE_SQLITE=False
+except ImportError:
+    sqlite3 = None
+
 
 def plugin(srv, item):
-    ''' sqlite. Expects addrs to contain (path, tablename) '''
+    """sqlite service plugin.
 
+    Records MQTT payload in an SQLite database table with a column called 'payload' of type TEXT.
+
+    Expects addrs to contain (path, tablename).
+
+    """
     srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
 
-    if HAVE_SQLITE is False:
-        srv.logging.warn("Sqlite3 is not installed")
+    if sqlite3 is None:
+        srv.logging.warn("sqlite3 is not installed.")
         return False
 
-    path  = item.addrs[0]
+    path = item.addrs[0]
     table = item.addrs[1]
+
     try:
         conn = sqlite3.connect(path)
-    except Exception, e:
-        srv.logging.warn("Cannot connect to sqlite at %s : %s" % (path, str(e)))
+    except sqlite3.Error as exc:
+        srv.logging.warn("Cannot connect to sqlite at '%s': %s", path, exc)
         return False
 
-    c = conn.cursor()
     try:
-        c.execute('CREATE TABLE IF NOT EXISTS %s (payload TEXT)' % table)
-    except Exception, e:
-        srv.logging.warn("Cannot create sqlite table in %s : %s" % (path, str(e)))
+        with conn:
+            conn.execute('CREATE TABLE IF NOT EXISTS "%s" (payload TEXT)' % table)
+    except sqlite3.Error as exc:
+        srv.logging.warn("Cannot create sqlite table '%s' in '%s': %s", table, path, exc)
+        conn.close()
         return False
 
-    text = item.message
-
     try:
-        c.execute('INSERT INTO %s VALUES (?)' % table, (text, ))
-        conn.commit()
-        c.close()
-    except Exception, e:
-        srv.logging.warn("Cannot INSERT INTO sqlite:%s : %s" % (table, str(e)))
+        with conn:
+            conn.execute('INSERT INTO "%s" VALUES (?)' % table, (item.message,))
+    except sqlite3.Error as exc:
+        srv.logging.warn("Cannot INSERT INTO sqlite table '%s': %s", table, exc)
+    finally:
+        conn.close()
 
     return True
