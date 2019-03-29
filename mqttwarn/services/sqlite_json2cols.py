3620d26f3bb880c4e99c2cb253d1e1b2d71d3f77
Christopher Arndt <chris@chrisarndt.de>
2019-03-29 22:28:58 +0100

    Almost complete rewrite of 'sqlite_json2cols' plugin; thereby also fixing Python 3 incompatibilities, outdated idioms and coding-style issues
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- mqttwarn/services/sqlite_json2cols.py
+++ mqttwarn/services/sqlite_json2cols.py
@@ -1,91 +1,118 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Vium https://github.com/Vium'
-__copyright__ = 'Copyright 2016'
-__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+__author__ = "Vium <https://github.com/Vium>, Christopher Arndt <info@chrisarndt.de>"
+__copyright__ = "Copyright 2016, 2019"
+__license__ = "Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"
 
-#based on the great SQLITE code by Jan-Piet Mens
-
-HAVE_SQLITE=True
-try:
-    import sqlite3
-except:
-    HAVE_SQLITE=False
+# Based on the great SQLITE code by Jan-Piet Mens
 
 import unicodedata
 
-def plugin(srv, item):
-    ''' sqlite. Expects addrs to contain (path, tablename) '''
+import six
 
+try:
+    import sqlite3
+except ImportError:
+    sqlite3 = None
+
+
+def normalize_unicode(s, encoding='ascii'):
+    return unicodedata.normalize('NFKD', s).strip().encode(encoding, 'ignore').decode(encoding)
+
+
+def plugin(srv, item):
+    """sqlite_json2cols service plugin.
+
+    Expects addrs to contain (path, tablename) and payload to be a JSON dict with string
+    or numeric values.
+
+    """
     srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
 
-    if HAVE_SQLITE is False:
-        srv.logging.warn("Sqlite3 is not installed")
+    if sqlite3 is None:
+        srv.logging.warn("sqlite3 is not installed")
         return False
-    path  = item.addrs[0]
+
+    path = item.addrs[0]
     table = item.addrs[1]
+    data = item.data
+
+    if not data or not isinstance(data, dict):
+        srv.logging.warn("Incorrect payload format (must be dict).")
+
+    col_names = []
+    col_definitions = []
+    col_values = []
+
+    # Determine column types from MQTT payload JSON data.
+    # Data is expected to be a dict mapping column names to values, e.g.
+    # {"sensor_id": "testsensor", "whatdata": "hello", "data": 1}
+    for key in data:
+        srv.logging.debug("Key: %r", key)
+        key = key.strip()
+
+        if not key or key.startswith('_') or key in ('payload', 'topic'):
+            # We just want to save the payload.
+            # There is probably a better way
+            continue
+
+        value = data[key]
+
+        if isinstance(value, six.integer_types):
+            col_type = 'float'
+        elif isinstance(value, six.string_types):
+            col_type = 'varchar(20)'
+
+        col_name = normalize_unicode(key)
+
+        if col_name:
+            col_definitions.append('"%s" %s' % (col_name, col_type))
+            col_names.append(col_name)
+            col_values.append(value)
+
+    if not col_names:
+        srv.logging.warn("No valid column fields found in payload")
+        return False
+
+    # Derive SQL table column definition string in case the table has to be created
+    # i.e. 'key1 varchar(20), key2 varchar(20), key3 float'
+    col_definitions = ", ".join(col_definitions)
+
+    # Collect column names for INSERT query string
+    col_names = ', '.join('"%s"' % name for name in col_names)
+
+    # Collect column values and build INSERT query placeholder string
+    col_placeholders = ", ".join(["?"] * len(col_values))
+
     try:
         conn = sqlite3.connect(path)
-    except Exception, e:
-        srv.logging.warn("Cannot connect to sqlite at %s : %s" % (path, str(e)))
-        return False
-
-
-
-
-    table_cols =  {}
-    #determine datatypes
-    for key in item.data: #i.e. {"sensor_id":"testsensor","whatdata":"hello","data":1}"
-        if key[0] == '_' or key == 'payload' or key == 'topic': #we just want to save the payload there is probably a better way
-            continue
-        else:
-            if isinstance(item.data[key],  (int, long, float)):
-                table_cols[key] = 'float'
-                item.data[key] = str(item.data[key]) #make str for sql
-            elif isinstance(item.data[key], basestring):
-                table_cols[key] = 'varchar(20)'
-                item.data[key] = "\'%s\'" % item.data[key] #add '' for sql
-
-    # derive sql rudimentary definition string in case the table has to be created
-    # i.e. 'key1 varchae(20), key2 varchar(20), key3 float'
-    col_definition = ''
-    for col in table_cols:
-        col_definition = col_definition + col + ' ' + table_cols[col] +','
-    col_definition = col_definition[:-1]
-    col_definition= unicodedata.normalize('NFKD', col_definition).encode('ascii','ignore')
-
-    #derive insert into string
-    # i.e. 'key1, key2, key3'
-    col_insert_into = ''
-    for val in table_cols:
-        col_insert_into  = col_insert_into + val + ','
-    col_insert_into  = col_insert_into[:-1]
-    col_insert_into= unicodedata.normalize('NFKD', col_insert_into).encode('ascii','ignore')
-
-    #derive value string
-    # i.e. '12, "hello", 23'
-    col_values = ''
-    for val in table_cols:
-        col_values = col_values + item.data[val] + ','
-    col_values = col_values[:-1]
-    col_values= unicodedata.normalize('NFKD', col_values).encode('ascii','ignore')
-
-    c = conn.cursor()
-    try:
-        srv.logging.debug("Inserting into SQLITE")
-        c.execute('CREATE TABLE IF NOT EXISTS %s (%s);' % (table, col_definition))
-    except Exception, e:
-        srv.logging.warn("Cannot create sqlite table in %s : %s" % (path, str(e)))
+    except sqlite3.Error as exc:
+        srv.logging.warn("Cannot connect to sqlite at '%s': %s", path, exc)
         return False
 
     try:
-        c.execute("INSERT INTO %s (%s) VALUES (%s);" % (table, col_insert_into, col_values))
-        conn.commit()
-        c.close()
-        srv.logging.debug("Inserted into SQLITE")
-    except Exception, e:
-        srv.logging.warn("Cannot INSERT INTO sqlite:%s : %s" % (table, str(e)))
+        query = 'CREATE TABLE IF NOT EXISTS "%s" (%s);' % (table, col_definitions)
+        srv.logging.debug("Creating SQLite table: %s", query)
+        with conn:
+            conn.execute(query)
+    except sqlite3.Error as exc:
+        srv.logging.warn("Cannot create sqlite table '%s' in '%s': %s", table, path, exc)
+        conn.close()
+        return False
+    else:
+        srv.logging.debug("SQLite CREATE TABLE sucessful")
 
+    try:
+        query = 'INSERT INTO "%s" (%s) VALUES (%s);' % (table, col_names, col_placeholders)
+        srv.logging.debug("Insert into SQLite: %s %% %r", query, tuple(col_values))
+        with conn:
+            conn.execute(query, col_values)
+    except sqlite3.Error as exc:
+        srv.logging.warn("Cannot INSERT INTO sqlite table '%s': %s", table, exc)
+    else:
+        srv.logging.debug("SQLite INSERT successful.")
+    finally:
+        conn.close()
 
     return True
