877afd09a12e3e20b7e8104a13e07d52fc58de58
Christopher Arndt <chris@chrisarndt.de>
2019-03-29 20:02:57 +0100

    Fix Python 3 incompatibilities, outdated idioms and coding-style issues in 'log' plugin
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- mqttwarn/services/log.py
+++ mqttwarn/services/log.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
-__copyright__ = 'Copyright 2014 Jan-Piet Mens'
-__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+__author__ = "Jan-Piet Mens <jpmens()gmail.com>"
+__copyright__ = "Copyright 2014 Jan-Piet Mens"
+__license__ = "Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"
+
 
 def plugin(srv, item):
-
+    """log service plugin."""
     srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
-
     level = item.addrs[0]
-
     text = item.message
 
     levels = {
-        'debug' : srv.logging.debug,
-        'info'  : srv.logging.info,
-        'warn'  : srv.logging.warning,
-        'crit'  : srv.logging.critical,
-        'error'  : srv.logging.error,
+        'debug': srv.logging.debug,
+        'info': srv.logging.info,
+        'warn': srv.logging.warning,
+        'crit': srv.logging.critical,
+        'error': srv.logging.error,
     }
 
     try:
         levels[level]("%s", text)
-    except Exception, e:
-        srv.logging.warn("Cannot invoke service log with level `%s': %s" % (level, str(e)))
+    except Exception as exc:
+        srv.logging.warn("Cannot invoke service log with level `%s': %s", level, exc)
         return False
 
     return True
