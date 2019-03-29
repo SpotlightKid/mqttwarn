f0b7c48a1a34ac8270b2b2e72a8457fe08f812fa
Christopher Arndt <chris@chrisarndt.de>
2019-03-29 20:00:16 +0100

    Fix Python 3 incompatibilities, outdated idioms and coding-style issues in 'file' plugin
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- mqttwarn/services/file.py
+++ mqttwarn/services/file.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
-__copyright__ = 'Copyright 2014 Jan-Piet Mens'
-__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+__author__ = "Jan-Piet Mens <jpmens()gmail.com>"
+__copyright__ = "Copyright 2014 Jan-Piet Mens"
+__license__ = "Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"
+
 
 def plugin(srv, item):
-
+    """file service plugin."""
     srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
-
-    mode = "a"
+    mode = 'a'
 
     # item.config is brought in from the configuration file
-    config   = item.config
+    config = item.config
 
     # addrs is a list[] associated with a particular target.
     # While it may contain more than one item (e.g. pushover)
@@ -23,17 +23,18 @@ def plugin(srv, item):
     # else the original payload
     text = item.message
 
-    if type(config) == dict and 'append_newline' in config and config['append_newline']:
-        text = text + "\n"
-    if type(config) == dict and 'overwrite' in config and config['overwrite']:
-        mode = "w"
+    if isinstance(config, dict):
+        if config.get('append_newline'):
+            text = text + '\n'
+
+        if config.get('overwrite'):
+            mode = 'w'
 
     try:
-        f = open(filename, mode)
-        f.write(text)
-        f.close()
-    except Exception, e:
-        srv.logging.warning("Cannot write to file `%s': %s" % (filename, str(e)))
+        with open(filename, mode) as fp:
+            fp.write(text)
+    except Exception as exc:
+        srv.logging.warning("Cannot write to file `%s': %s", filename, exc)
         return False
 
     return True
