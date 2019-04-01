c75099a8a16b4be47b17ce64aca5decc18fc3d24
Christopher Arndt <chris@chrisarndt.de>
2019-04-01 20:32:12 +0200

    Cleaned up 'smtp' service plugin:
    * Added 'usessl' and 'debuglevel' options
    * Renamed 'htmlmsg' option to 'html'
    * Set default values for all options where sensible
    * Add check if 'sender' option is set
    * Fixed Python 3 incompatibilities
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- mqttwarn/services/smtp.py
+++ mqttwarn/services/smtp.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = 'Jan-Piet Mens <jpmens()gmail.com>'
-__copyright__ = 'Copyright 2014 Jan-Piet Mens'
-__license__   = """Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"""
+__author__ = "Jan-Piet Mens <jpmens()gmail.com>, Christopher Arndt <info@chrisarndt.de>"
+__copyright__ = "Copyright 2014 Jan-Piet Mens, 2019 Christopher Arndt"
+__license__ = "Eclipse Public License - v 1.0 (http://www.eclipse.org/legal/epl-v10.html)"
+
 
 import smtplib
 from email.mime.text import MIMEText
@@ -12,48 +12,69 @@ from email.mime.multipart import MIMEMultipart
 
 def plugin(srv, item):
     """Send a message to SMTP recipient(s)."""
+    service = item.service
+    target = item.target
+    topic = item.topic
 
-    srv.logging.debug("*** MODULE=%s: service=%s, target=%s", __file__, item.service, item.target)
+    srv.log.debug("*** MODULE=%s: service=%s, target=%s", __file__, service, target)
 
-    smtp_addresses = item.addrs
+    conf = item.config.get
+    sender = conf('sender')
 
-    server      = item.config['server']
-    sender      = item.config['sender']
-    starttls    = item.config['starttls']
-    username    = item.config['username']
-    password    = item.config['password']
+    if not sender:
+        srv.log.warn("Skipping SMTP notification for service '%s': no sender configured.", service)
+        return False
 
-    if 'htmlmsg' in item.config:
+    recipients = item.addrs
+
+    if not recipients:
+        srv.log.warn("Skipping SMTP notification for service '%s:%s' on topic '%s': "
+                     "no recipients configured.", service, target, topic)
+        return False
+
+    server = conf('server', 'localhost')
+    usessl = conf('usessl', False)
+    starttls = conf('starttls', False)
+    port = conf('port', 465 if usessl else 587)
+    username = conf('username')
+    password = conf('password')
+
+    if conf('html', False):
         msg = MIMEMultipart('alternative')
         msg.attach(MIMEText(item.message, 'plain'))
         msg.attach(MIMEText(item.message, 'html'))
     else:
         msg = MIMEText(item.message, 'plain')
-    msg['Subject']      = item.get('title', "%s notification" % (srv.SCRIPTNAME))
-    msg['To']           = ", ".join(smtp_addresses)
-    msg['From']         = sender
-    msg['X-Mailer']     = srv.SCRIPTNAME
 
-    if not smtp_addresses:
-        srv.logging.warn("Skipped sending SMTP notification to %s, "
-                         "no addresses configured" % (item.target))
-        return False
+    msg['Subject'] = item.get('title', srv.SCRIPTNAME + " notification")
+    msg['To'] = ", ".join(recipients)
+    msg['From'] = sender
+    msg['X-Mailer'] = srv.SCRIPTNAME
 
     try:
-        srv.logging.debug("Sending SMTP notification to %s, addresses: %s" % (item.target, smtp_addresses))
-        server = smtplib.SMTP(server)
-        server.set_debuglevel(0)
+        srv.log.debug("Sending SMTP notification for service '%s:%s' on topic '%s'. "
+                      "Recipients: %r", service, target, topic, recipients)
+
+        if usessl:
+            server = smtplib.SMTP_SSL(server, port)
+        else:
+            server = smtplib.SMTP(server, port)
+
+        server.set_debuglevel(conf('debuglevel', 0))
         server.ehlo()
-        if starttls:
+
+        if not usessl and starttls:
             server.starttls()
+
         if username:
-            server.login(str(username), str(password))
-        server.sendmail(sender, smtp_addresses, msg.as_string())
+            server.login(username, password)
+
+        server.sendmail(sender, recipients, msg.as_string())
         server.quit()
-        srv.logging.debug("Successfully sent SMTP notification")
-    except Exception, e:
-        srv.logging.warn("Error sending notification to SMTP recipient %s, addresses: %s. "
-                         "Exception: %s" % (item.target, smtp_addresses, str(e)))
+        srv.log.info("Successfully sent SMTP notification.")
+    except Exception as exc:
+        srv.log.warn("Error sending SMTP notification for service '%s:%s', on topic '%s'. "
+                     "Recipients: %r Error: %s", service, target, recipients, exc)
         return False
 
     return True
